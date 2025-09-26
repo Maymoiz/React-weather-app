857006f53fdd4f5569ebbf0078fe2145ed8f5946
Maymoiz <208803754+Maymoiz@users.noreply.github.com>
2025-09-26 07:43:08 +0200

    modified react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <footer>This project was coded by Moisha Ndlovu and is <a href='https://github.com/Maymoiz/React-weather-app' target='_blank'>open-sourced on Github.</a></footer>
+      <footer>This project was coded by Moisha Ndlovu and is <a href='https://github.com/Maymoiz/React-weather-app' target='_blank' rel='noreferrer'>open-sourced on Github.</a></footer>
     </div>
   );
 }
